1677267b6888ec0334c6691c58e628e526a2854d
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-05 11:07:48 +0200

    working on the ruby excersice

--- ruby_excersises2.rb
+++ ruby_excersises2.rb
@@ -1,9 +1,21 @@
 
-family = {  uncles: ["bob", "joe", "steve"],
+family = {  
+
+    uncles: ["bob", "joe", "steve"],
     sisters: ["jane", "jill", "beth"],
     brothers: ["frank", "rob", "david"],
     aunts: ["mary", "sally", "susan"]
 }
+
+
+
+    immediate_family = family.select  do |siblings|
+        siblings== :sisters || siblings == :brothers
+    end
+
+    array = immediate_family.values.flatten
+    puts  array
+
 
 
 hash2 = Hash.new
